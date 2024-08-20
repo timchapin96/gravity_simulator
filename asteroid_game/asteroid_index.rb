e4c5791c8f4509c2ad9783a676476c27c811cb6a
Tim Chapin <chapin.timothys@gmail.com>
2024-08-20 11:42:23 +0900

    Fixed movement glitch in the gravity zone

--- asteroid_game/asteroid_index.rb
+++ asteroid_game/asteroid_index.rb
@@ -18,7 +18,7 @@ jump_strength = 4
 
 background = Image.new(
   '../asteroid_game/images/space_background.jpeg',
-  width: 1920, height: 1000, resizable: true,
+  width: 1920, height: 1000,
   z: 0
 )
 
@@ -27,8 +27,7 @@ Planet1 = Circle.new(
   radius: planet1_radius,
   sectors: 120,
   color: 'blue',
-  z: 2,
-  space_background: background
+  z: 2
 )
 Planet1_gravity_field = Circle.new(
   x: Window.width / 2, y: Window.height / 2,
@@ -41,8 +40,10 @@ Planet1_gravity_field = Circle.new(
 
 small_circle1 = Asteroid.new(asteroid_radius)
 
+
 # Left and right arrow key to move around circle
 on :key_held do |event|
+  puts(degrees)
   # If asteroid leaves gravity field call move_in_space method
   if !(Planet1_gravity_field.contains? small_circle1.body.x, small_circle1.body.y) && (event.key == 'left')
     degrees -= small_circle1.speed
@@ -51,14 +52,17 @@ on :key_held do |event|
     degrees += small_circle1.speed
     move_in_space(small_circle1.body, Planet1, degrees, $new_x_position, $new_y_position)
   # If asteroid is in gravity field call move method
+  # Recalculate where the asteroid landed and move from there with degrees-bet_coor func
   elsif event.key == 'left'
+    degrees = degrees_between_coordinates(Planet1.x, Planet1.y, small_circle1.body.x, small_circle1.body.y)
     degrees -= small_circle1.speed
     move(small_circle1.body, Planet1, degrees)
   elsif event.key == 'right'
+    degrees = degrees_between_coordinates(Planet1.x, Planet1.y, small_circle1.body.x, small_circle1.body.y)
     degrees += small_circle1.speed
     move(small_circle1.body, Planet1, degrees)
   end
-  puts(small_circle1.body.x)
+  # puts(small_circle1.body.x)
 end
 
 on :key_down do |event|
